def359b174756f030076dde20df907f11e1dae5c
csjcode <christophersstjohn@gmail.com>
2016-04-26 17:29:54 +0700

    Added Api module, Fetch w/ promises

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,11 +1,10 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 var Header = require('./header');
 
-var renderCount = 0;
-
+console.log('main');
 module.exports = React.createClass({
    render: function (){
-      console.log (renderCount++);
       return <div>
          <Header />
          {this.props.childen}
---
+++ src/utils/api.jsx
@@ -0,0 +1,20 @@
+var Fetch = require('whatwg-fetch');
+var rootUrl = 'https://api.imgur.com/3/';
+var apiKey = '9f25d4555820f04';
+console.log('api');
+
+module.exports = window.api = {
+  get: function(url) {
+    return fetch(rootUrl + url, {
+      headers: {
+        'Authorization': 'Client-ID ' + apiKey
+      }
+    })
+    .then(function(response){
+      return response.json()
+    })
+    // .then(function(data){ // to print array of data to console
+    //   console.log(data);
+    })
+  }
+};
