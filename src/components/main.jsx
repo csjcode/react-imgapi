6db676d5f4cc8557872b6ee25601f7dce247ef0d
csjcode <christophersstjohn@gmail.com>
2016-04-27 14:18:53 +0700

    Added topic-list component

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,13 +1,21 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Header = require('./header');
+var TopicList = require('./topics-list');
 
 console.log('main');
 module.exports = React.createClass({
    render: function (){
       return <div>
          <Header />
-         {this.props.childen}
+         {this.content()}
       </div>
+   },
+   content: function() {
+      if (this.props.children){
+         return this.props.children
+      } else {
+         return <TopicList />
+      }
    }
-})
+});
