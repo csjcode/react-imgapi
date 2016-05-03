0e955096643fbbc59c4851d958777c70c392f00c
csjcode <christophersstjohn@gmail.com>
2016-05-03 11:00:10 +0700

    Added Routing parameters for Topic page

---
+++ src/components/topic.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+// var Header = require('./header');
+
+module.exports = React.createClass({
+   render: function () {
+      return <div>
+         I am a Topic
+      </div>
+   }
+})
