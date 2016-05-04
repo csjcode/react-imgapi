89b22166f70a70ea7d6aae082f52e156646deec6
csjcode <christophersstjohn@gmail.com>
2016-05-05 00:47:54 +0700

    Added image/topic route, image store and api call

--- src/components/topic.jsx
+++ src/components/topic.jsx
@@ -1,11 +1,29 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ImageStore = require('../stores/image-store');
+var Actions = require('../actions');
+var Reflux = require('reflux');
 // var Header = require('./header');
 
 module.exports = React.createClass({
-   render: function () {
+   mixins: [
+      Reflux.listenTo(ImageStore, 'onChange')
+   ],
+   getInitialState: function() {
+      return {
+         images:[]
+      }
+   },
+   componentWillMount: function() {
+      Actions.getImages(this.props.params.id)
+   },
+   render: function() {
       return <div>
-         I am a Topic
+         // I am a Topic with ID {this.props.params.id}
+
       </div>
+   },
+   onChange: function(event,images) {
+      this.setState({images:images})
    }
-})
+});
