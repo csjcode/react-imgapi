615ecb32151f127732f117d4c100b50edf0015e8
csjcode <christophersstjohn@gmail.com>
2016-05-05 17:19:27 +0700

    Added api  call by id, fixed Link url to stop double api call

--- src/components/topic.jsx
+++ src/components/topic.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-var ReactDOM = require('react-dom');
+// var ReactDOM = require('react-dom');
 var ImageStore = require('../stores/image-store');
 var Actions = require('../actions');
 var Reflux = require('reflux');
@@ -15,9 +15,15 @@ module.exports = React.createClass({
       }
    },
    componentWillMount: function() {
+      // console.log('Topic is about to render and fetch data');
       Actions.getImages(this.props.params.id)
    },
+   componentWillReceiveProps: function (nextProps) {
+      Actions.getImages(nextProps.params.id)
+   },
    render: function() {
+      // console.log('Topic rendering with ID ' + this.props.params.id);
+      // console.log('I have this many images ' + this.state.images.length);
       return <div>
          // I am a Topic with ID {this.props.params.id}
 
