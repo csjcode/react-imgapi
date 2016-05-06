a200529c0cfe54a639a89fde324ed90a6e1d4f7d
csjcode <christophersstjohn@gmail.com>
2016-05-06 22:46:59 +0700

    Added play styling and info inset

--- src/components/image-preview.jsx
+++ src/components/image-preview.jsx
@@ -13,7 +13,16 @@ module.exports = React.createClass({
       onMouseLeave={this.handleMouseLeave}
       >
       {this.props.animated && this.state.hovering ? this.video() : this.image()}
+      {this.props.animated && !this.state.hovering ? this.icon() : null}
+      {this.state.hovering ? this.inset() : null }
     </div>
+  },
+  inset: function () {
+    return <div className="inset">
+      Views: {this.props.views}
+      <br />
+      Upvotes: {this.props.ups}
+    </div>;
   },
   image: function() {
     var link = 'http://i.imgur.com/' + this.props.id + 'h.jpg';
@@ -26,6 +35,11 @@ module.exports = React.createClass({
         <source src={this.props.mp4} type='video/mp4'></source>
       </video>
     </div>
+  },
+  icon: function () {
+    return <div>
+      <span className="glyphicon glyphicon-play"></span>
+    </div>;
   },
   handleMouseEnter: function() {
     this.setState({hovering: true});
